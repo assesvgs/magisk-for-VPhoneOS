542a07c586382040eb460e5294451cea95f41847
Developer <developer@example.com>
2026-06-11 23:39:03 +0800

    fix: 添加缺失的 UI 字符串，修复 DenyListFragment sulist 标题切换

--- app/apk/src/main/java/com/topjohnwu/magisk/ui/deny/DenyListFragment.kt
+++ app/apk/src/main/java/com/topjohnwu/magisk/ui/deny/DenyListFragment.kt
@@ -27,7 +27,8 @@ class DenyListFragment : BaseFragment<FragmentDenyMd2Binding>(), MenuProvider {
 
     override fun onStart() {
         super.onStart()
-        activity?.setTitle(CoreR.string.denylist)
+        if (Info.sulist) activity?.setTitle(CoreR.string.settings_sulist)
+        else activity?.setTitle(CoreR.string.settings_magiskhide_title)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
