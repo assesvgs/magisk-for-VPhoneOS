298749712d0e9c7c738a92d4ec19cd3b80e898d5
Developer <developer@example.com>
2026-06-11 20:51:26 +0800

    fix: 更新 magisk 构建列表，替换 logcat.cpp 为 ptrace.cpp/revert.cpp/kitsune.cpp

--- native/src/Android.mk
+++ native/src/Android.mk
@@ -27,7 +27,9 @@ LOCAL_SRC_FILES := \
     core/zygisk/hook.cpp \
     core/deny/cli.cpp \
     core/deny/utils.cpp \
-    core/deny/logcat.cpp
+    core/deny/ptrace.cpp \
+    core/deny/revert.cpp \
+    core/kitsune.cpp
 
 LOCAL_LDLIBS := -llog
 LOCAL_LDFLAGS := -Wl,--dynamic-list=src/exported_sym.txt
